24a7f9a2a3ae4dbfe22ee56059737039bc04a921
Wu-Pu-Hui-Qian-Ren <113888878+Wu-Pu-Hui-Qian-Ren@users.noreply.github.com>
2025-01-08 19:31:06 +0800

    upload latest version with yolo&segment$igev

--- YOLO/export.py
+++
@@ -1,7 +0,0 @@
-from ultralytics import YOLO
-
-# Load a YOLOv8 model
-model = YOLO('YOLO/yolov8x.pt')
-
-# Export the model （set your parameters, refer to ultralytics）
-model.export(format="engine", opset=12, simplify=True, dynamic=True, int8=True,imgsz=640,half=True)
